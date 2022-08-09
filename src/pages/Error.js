aa6617fbe8991ed5e2a5bf09a3ca26c2736571f9
KIRILL F <tiltlessman@gmail.com>
2022-08-09 12:31:45 +0300

    3-error-page-complete

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,49 @@
 import React from 'react'
+import error from '../assets/not-found.svg'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Error = () => {
-  return <div>Error</div>
+  return (
+    <Wrapper>
+      <div>
+        <img src={error} alt='error' className='error-img' />
+        <div className='info-container'>
+          <div>
+            <h1>Ошибка 404</h1>
+            <p className='info-p'>Страница не найдена. Попробуйте снова!</p>
+            <Link to='/' className='btn'>
+              На главную
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.div`
+  width: var(--min-width);
+  max-width: var(--max-width);
+  margin: 0 auto;
+  height: 95vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .info-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+  .info-p {
+    margin-bottom: 2.5rem;
+  }
+  @media screen and (max-width: 900px) {
+    .error-img {
+      display: none;
+    }
+  }
+`
 
 export default Error
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -36,21 +36,6 @@ const Wrapper = styled.div`
     width: 500px;
     margin-right: 5rem;
   }
-  .btn {
-    cursor: pointer;
-    text-decoration: none;
-    font-family: 'Ubuntu', sans-serif;
-    border: none;
-    font-size: 1.25rem;
-    border-radius: 5px;
-    padding: 0.5rem;
-    background: var(--red);
-    color: var(--white);
-  }
-  .btn:hover {
-    transition: var(--transition);
-    background: var(--dark-red);
-  }
   @media screen and (max-width: 900px) {
     .main-img {
       display: none;
